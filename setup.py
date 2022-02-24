2c6db41eb5bfc9771bb4d774524b24388f829efe
vasanth356 <chanukya.vasanth7@gmail.com>
2022-02-24 14:51:10 +0000

    stage 02

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 from setuptools import setup, find_packages
 
-with open ("README.md","r",encoding = "utf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name = 'src',
-    version = "0.0.1",
-    author = "c17hawke", description = "A small package for dvc ml pipeline demo",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/vasanth356/simple_dvc_project"
-    author_email = "chanukya.vasanth7@gmail.com",
-    package_dir = {"": "src"},
-    packages = find_packages(where = "src"), license = "GNU",
-    python_requires = ">=3.6",
-    installation_requires = (
-    'dvc',
-    'dvc[gdrive]',
-    'dvc[s3]',
-    'pandas',
-     'scikit-learn')
+    name="src",
+    version="0.0.3",
+    author="c17hawke",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vasanth356/simple_dvc_project",
+    author_email="chanukya.vasanth7@gmail.com",
+    # package_dir={"": "src"},
+    # packages=find_packages(where="src"),
+    packages=["src"],
+    license="GNU",
+    python_requires=">=3.6",
+    install_requires=[
+        'dvc',
+        'dvc[gdrive]',
+        'dvc[s3]',
+        'pandas',
+        'scikit-learn'
+    ]
 )
